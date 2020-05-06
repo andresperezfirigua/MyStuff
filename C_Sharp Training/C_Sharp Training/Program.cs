768d3ecbed65ea7035538d44946a90283e9d224f
Andrés Pérez <andresperezfirigua@gmail.com>
2020-05-05 19:45:25 -0500

    Primer commit

---
+++ C_Sharp Training/C_Sharp Training/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Sharp_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 1;
+            if (a==b)
+            {
+                Console.WriteLine("Hello World!");
+            }else
+            {
+                Console.WriteLine("It´s not the same");
+            }
+        }
+    }
+} 
